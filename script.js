694ccbaa3268ee46832db2d6c9b67c78cf7108a7
Dev-Gitanjali <jgitanjali592@gmail.com>
2024-02-21 10:52:17 +0530

    this method for adding eventlistener

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const username = document.getElementById('username')
 const email = document.getElementById('email')
 const password = document.getElementById('password')
 const password2= document.getElementById('password2')
+// show input error message
+function showError(input,message){
+
+}
+
+// event listener
+form.addEventListener('submit' ,function(e){
+    e.preventDefault();
+      
+    if(username.value ===''){
+        showError( username,'username is required');
+    
+    } else{
+        showSuccess(username);
+    }
+});
